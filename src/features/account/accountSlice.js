988a2baeacc8b479b436ee24017c6add8ff69918
Naser Ahadi <n.ahadi@vandar.io>
2025-06-08 23:52:47 +0330

    implement async operation by redux-thunk middleware

--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -2,12 +2,17 @@ const initialAccountState = {
   balance: 0,
   loan: 0,
   loanPurpose: 0,
+  isLoading: false,
 };
 
 export default function accountReducer(state = initialAccountState, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
@@ -25,13 +30,26 @@ export default function accountReducer(state = initialAccountState, action) {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
+    case "account/convertingCurrency":
+      return { ...state, isLoading: true };
     default:
       return state;
   }
 }
 
-function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    const response = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`,
+    );
+    const data = await response.json();
+    console.log(data);
+    const converted = data.rates.USD;
+    dispatch({ type: "account/deposit", payload: converted });
+  };
 }
 
 function withdraw(amount) {
