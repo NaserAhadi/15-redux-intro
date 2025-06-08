83d4c5370982293aec76b162ccaad0d1af688bd2
Naser Ahadi <n.ahadi@vandar.io>
2025-06-08 16:29:54 +0330

    change the structure of store in to slice states

---
+++ src/features/customer/customerSlice.js
@@ -0,0 +1,34 @@
+const initialCustomerState = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initialCustomerState, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
+}
+
+export { createCustomer, updateName };
