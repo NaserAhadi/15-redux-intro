21ffbe1dc949f06b4d7a97bced538b0ea124570b
Naser Ahadi <n.ahadi@vandar.io>
2025-06-08 23:51:52 +0330

    apply redux-thunk middleware

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customer/customerSlice";
+import { thunk } from "redux-thunk";
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 export default store;
