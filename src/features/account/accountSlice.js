63760ccdd62b6c1ef512db5315b774c2b41f9f9d
Naser Ahadi <n.ahadi@vandar.io>
2025-06-09 15:40:06 +0330

    implement slice based on redux toolkit

--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -1,10 +1,73 @@
-const initialAccountState = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   balance: 0,
   loan: 0,
-  loanPurpose: 0,
+  loanPurpose: "",
   isLoading: false,
 };
 
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      console.log("deposit - 1");
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: {
+            amount,
+            purpose,
+          },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+
+        state.loan = action.payload.amount;
+        state.balance += action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+      },
+    },
+    payLoan(state) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+    convertingCurrency(state) {
+      console.log("converting Currency state", state);
+      state.isLoading = true;
+    },
+  },
+});
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    console.log("deposit - async");
+    dispatch({ type: "account/convertingCurrency" });
+    const response = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`,
+    );
+    const data = await response.json();
+    console.log(data);
+    const converted = data.rates.USD;
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
+
+export default accountSlice.reducer;
+console.log(accountSlice);
+/*
 export default function accountReducer(state = initialAccountState, action) {
   switch (action.type) {
     case "account/deposit":
@@ -68,3 +131,4 @@ function payloan() {
 }
 
 export { deposit, withdraw, requestLoan, payloan };
+ */
--- src/features/customer/customerSlice.js
+++ src/features/customer/customerSlice.js
@@ -1,9 +1,40 @@
-const initialCustomerState = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+/*
 export default function customerReducer(state = initialCustomerState, action) {
   switch (action.type) {
     case "customer/createCustomer":
@@ -32,3 +63,4 @@ function updateName(fullName) {
 }
 
 export { createCustomer, updateName };
+ */
