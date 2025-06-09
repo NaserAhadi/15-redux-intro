c8797217bbd42005a70ed01d5ac4efd895d8ec8e
Naser Ahadi <n.ahadi@vandar.io>
2025-06-09 15:38:47 +0330

    configure store with redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,14 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customer/customerSlice";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "@redux-devtools/extension";
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
-);
+
 export default store;
